4ca192933c82065f25543d3ab2816f73f0740d93
awhit3 <107559380+42awhite@users.noreply.github.com>
2024-10-01 15:14:05 +0200

    ex00 done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -3,16 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ablanco- <ablanco-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pc <pc@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 19:53:29 by ablanco-          #+#    #+#             */
-/*   Updated: 2024/09/29 19:56:26 by ablanco-         ###   ########.fr       */
+/*   Updated: 2024/10/01 14:48:50 by pc               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+//std::cout -> lanza por terminal
+//std::endl -> Salto de línea
+
 int main(int argc, char **argv)
 {
-    
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int n_arg = 1; n_arg < argc; n_arg++)
+		{
+			for (int idx = 0; argv[n_arg][idx]; idx++)
+				std::cout << (char)toupper(argv[n_arg][idx]);
+		}
+		std::cout << std::endl;
+	}
+    return(0);
 }
